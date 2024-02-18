1e9faa000b332444aedd3f52933805eb30570a66
ShinyGorilla <134111158+ShinyGorilla@users.noreply.github.com>
2024-02-18 13:18:34 -0700

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace MuteLavaWarning
+{
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		public static AudioSource Audio;
+		void Start()
+		{
+            GorillaTagger.OnPlayerSpawned(PlayerSpawned);
+        }
+
+		void OnEnable() => HarmonyPatches.ApplyHarmonyPatches();
+		void OnDisable() => HarmonyPatches.RemoveHarmonyPatches();
+
+		void PlayerSpawned()
+		{
+            Debug.Log("Mute Lava Warning Initialized");
+
+            Audio = GameObject.Find("Environment Objects/LocalObjects_Prefab/Forest/Terrain/SoundPostForest/geo/ForestSpeakerAudioSrc_Song2").GetComponent<AudioSource>();
+        }
+
+		void Update()
+		{
+            Audio.mute = true;
+        }
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace MuteLavaWarning
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "ShinyGorilla.MuteLavaWarning";
+		public const string Name = "ThePenisEek";
+		public const string Version = "1.0.0";
+	}
+}
